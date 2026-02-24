2d2d1d71bd265dae5615f1e1d215bbaffc6d51b6
Diego Vercelli <diegoev26@yahoo.com.ar>
2026-02-23 21:05:38 -0300

    Update sitemap.ts

--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -1,10 +1,8 @@
 import { MetadataRoute } from "next";
-import config from "@/config/config";
-const { publicUrl } = config;
 export const dynamic = "force-static";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = publicUrl;
+  const baseUrl = "http://localhost/";
 
   // 1. Rutas Estáticas
   const staticRoutes = [""].map((route) => ({
