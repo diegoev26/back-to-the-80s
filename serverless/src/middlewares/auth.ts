5affd3a727fc09d72a0e9285cd7d6c78588a8f84
Diego Vercelli <diegoev26@yahoo.com.ar>
2026-06-26 13:55:42 -0300

    Update auth.ts

--- serverless/src/middlewares/auth.ts
+++ serverless/src/middlewares/auth.ts
@@ -1,27 +1,20 @@
 import { Env } from "../types/env.js";
-import { getCorsHeaders } from "./cors.js";
+import { sendServerlessResponse } from "../utils/api.utils.js";
 
 export const validateAuth = (request: Request, env: Env): Response | null => {
   const authHeader = request.headers.get("Authorization");
-  const headers = getCorsHeaders(request);
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return new Response(
-      JSON.stringify({
-        error: "No se proporcionó un token de autorización válido",
-      }),
-      { status: 401, headers },
-    );
-  }
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    return sendServerlessResponse(request, 401, {
+      error: { message: "No se proporcionó un token de autorización válido" },
+    });
 
   const token = authHeader.split(" ")[1];
 
-  if (token !== env.CLIENT_API_SECRET) {
-    return new Response(
-      JSON.stringify({ error: "Token de acceso inválido o expirado" }),
-      { status: 403, headers },
-    );
-  }
+  if (token !== env?.CLIENT_API_SECRET)
+    return sendServerlessResponse(request, 403, {
+      error: { message: "Token de acceso inválido o expirado" },
+    });
 
   return null;
 };
