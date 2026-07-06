60a1b57553a4f9d10638796d4501d382bf7c5ce5
Diego Vercelli <diegoev26@yahoo.com.ar>
2026-07-06 14:16:17 -0300

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,6 @@ import CommunityCard from "@/components/ComunityCard";
 import Nav from "@/components/Nav";
 
 export const dynamic = "force-dynamic";
-export const runtime = "edge";
 
 export default function Page() {
   return (
