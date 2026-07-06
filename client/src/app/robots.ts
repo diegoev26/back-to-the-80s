4bb2ed6bf62a9975871996db52ac4e37a345ea12
Diego Vercelli <diegoev26@yahoo.com.ar>
2026-07-06 12:50:28 -0300

    Update robots.ts

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -1,7 +1,9 @@
 import type { MetadataRoute } from "next";
 import config from "@/config/env.config";
 const { url: publicUrl } = config.options;
+
 export const dynamic = "force-static";
+export const runtime = "edge";
 
 export default function robots(): MetadataRoute.Robots {
   return {
